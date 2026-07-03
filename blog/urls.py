210b42156892e4c44c443e205df3551be056a275
fatima <fatiimahoseini@gmail.com>
2026-07-03 21:55:18 +0330

    blog-detail base, needs editing

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.blog, name="blog"),
+    path('', views.blog, name='blog'),
+    # path('<slug:slug>/', views.blog_detail, name='blog_detail'),
+    path('preview/', views.blog_preview, name='blog_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def blog(request):
     return render(request, "blog/blog.html")
+
+def blog_preview(request):
+    return render(request, 'blog/blog_detail.html')
