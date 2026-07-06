1fd4f0fe9b6702e7683804bbfdee0697827b41ff
fatima <fatiimahoseini@gmail.com>
2026-07-06 20:43:33 +0330

    connect blog section in home page to blog model and add slider for posts

--- core/views.py
+++ core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from blog.models import Post
+
 
 def home(request):
-    return render(request, 'core/home.html')
+    blog_posts = Post.objects.all()[:6]
+    context = {"blog_posts": blog_posts}
+    return render(request, "core/home.html", context)
+
 
 def contact(request):
-    return render(request, 'core/contact.html')
+    return render(request, "core/contact.html")
